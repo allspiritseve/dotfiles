e0d8daa552d0951a44f3a3c10edfbc15a074d6da
Cory Kaufman-Schofield <cory@eriador.corykaufman.com>
2013-06-04 22:07:46 +0000

    Simplified install script

--- install.sh
+++ install.sh
@@ -3,17 +3,10 @@
 export DOTFILES="$HOME/.dotfiles"
 export VIMHOME="$HOME/.vim"
 
-if [ ! -d "$VIMHOME" ]; then mkdir $VIMHOME; fi
-
-if ! [ -e "$VIMHOME/autoload" ]; then
-  ln -s $DOTFILES/autoload $HOME/.vim/autoload
-fi
-
-if ! [ -e "$VIMHOME/bundle" ]; then
-  ln -s $DOTFILES/bundle $HOME/.vim/bundle
-fi
-
-ln -sf $DOTFILES/.ackrc $HOME/.ackrc
-ln -sf $DOTFILES/.gvimrc $HOME/.gvimrc
-ln -sf $DOTFILES/.inputrc $HOME/.inputrc
-ln -sf $DOTFILES/.vimrc $HOME/.vimrc
+test -d "$VIMHOME" || mkdir $VIMHOME
+test -h "$VIMHOME/autoload" || ln -nsf $DOTFILES/autoload $VIMHOME/autoload
+test -h "$VIMHOME/bundle" || ln -nsf $DOTFILES/bundle $VIMHOME/bundle
+test -h "$VIMHOME/.ackrc" || ln -nsf $DOTFILES/.ackrc $HOME/.ackrc
+test -h "$VIMHOME/.gvimrc" || ln -sf $DOTFILES/.gvimrc $HOME/.gvimrc
+test -h "$VIMHOME/.inputrc" || ln -sf $DOTFILES/.inputrc $HOME/.inputrc
+test -h "$VIMHOME/.vimrc" || ln -sf $DOTFILES/.vimrc $HOME/.vimrc
