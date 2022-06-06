e610dcd727538c76e76b9aa9c62adb1be5bb7133
Cory Kaufman-Schofield <cory@corykaufman.com>
2022-06-06 15:21:30 -0400

    Rename to install.bash

--- install.bash
+++ install.bash
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-if [[ -d ~/.vim/pack/themes/opt/solarized8 ]]; then
+make
+
+if [[ ! -d ~/.vim/pack/themes/opt/solarized8 ]]; then
+  echo "clone"
   git clone https://github.com/lifepillar/vim-solarized8.git \
       ~/.vim/pack/themes/opt/solarized8
 fi
+
+echo "done"
